5e647a1d1f620163341daa3cbb47a14fbfd721db
Nashit Humam <85383203+s-nashit@users.noreply.github.com>
2025-03-26 16:25:18 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,9 @@ import plotly.express as px
 
 df = sns.load_dataset('tips')
 
+
 app = dash.Dash(__name__)
+server = app.server
 
 app.layout = html.Div([
     html.H1('Tips Dataset Analysis'),
@@ -44,4 +46,4 @@ def update_graph(x_axis):
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port = 8050)
+    app.run(debug=True, port = 8050, host='0.0.0.0')
